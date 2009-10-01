70322d14fd5a0fc1365841bf9fa64d9eb95f3694
regis User <regis@regis-laptop.(none)>
2009-10-01 11:02:28 -0300

    alteracoes realizadas no dojophb de 25/09/2009

--- src/jogodavelha/JogoDaVelha.java
+++ src/jogodavelha/JogoDaVelha.java
@@ -1,25 +1,63 @@
 package jogodavelha;
 
+import static junit.framework.Assert.assertEquals;
+
 import java.util.Arrays;
- 
+
 public class JogoDaVelha {
   private char[][] tabuleiro;
-  
+
   public JogoDaVelha() {
     tabuleiro = new char[3][3];
-    for (int i=0; i < tabuleiro.length; i++) {
-      Arrays.fill(tabuleiro[i], ' ');
+    preencheTudo(' ');
+
+  }
+
+  public void preencheTudo(char elemento) {
+    for (int i = 0; i < tabuleiro.length; i++) {
+      Arrays.fill(tabuleiro[i], elemento);
     }
   }
-  
-  public char[][] getTabuleiro(){
-    return tabuleiro;
-  } 
 
-  public void preenchePosicao(int x,int y,char v){
-    tabuleiro[x][y] = v;
+  public char[][] getTabuleiro() {
+    return tabuleiro;
   }
-  public char obterPosicao(int x,int y){
+
+  public void preenchePosicao(int x, int y, char elemento) {
+    if(x<0 || x>2 || y<0 || y>2){
+      throw new IllegalArgumentException("posição invalida");
+    }
+    if (testeElementoValido(elemento)){
+      if (obterPosicao(x, y) == ' ') {
+        tabuleiro[x][y] = elemento;
+      }
+      else
+      {
+        throw new IllegalArgumentException("Posicao ja preenchida");
+      }
+    }
+    else{
+      throw new IllegalArgumentException("Valor invalido");
+    }
+
+  }
+
+  public char obterPosicao(int x, int y) {
     return tabuleiro[x][y];
+  }
+
+  private boolean testeElementoValido(char elemento) {
+    return elemento=='O'||elemento=='X';
+  }
+
+  public boolean checaTabuleiroCheio() {
+    for (int i = 0; i < getTabuleiro().length; i++) {
+      for (int j = 0; j < getTabuleiro()[i].length; j++) {
+        if (getTabuleiro()[i][j] == ' ') {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 }
--- test/jogodavelha/JogoDaVelhaTest.java
+++ test/jogodavelha/JogoDaVelhaTest.java
@@ -9,7 +9,7 @@ public class JogoDaVelhaTest {
   public void inicializa(){
     jogo = new JogoDaVelha();
   }
-     
+
   @Test
   public void testaTamanho() {
     assertEquals( 3, jogo.getTabuleiro().length);
@@ -17,8 +17,8 @@ public class JogoDaVelhaTest {
       assertEquals(3, jogo.getTabuleiro()[i].length);
     }
   }
-  
-  
+
+
   @Test
   public void testaInicializacao(){
     for (int i = 0; i < jogo.getTabuleiro().length; i++) {
@@ -27,11 +27,40 @@ public class JogoDaVelhaTest {
       }
     }
   }
-  
+
   @Test
   public void testePreenchimentoPosicao() {
     jogo.preenchePosicao(1, 1, 'O');
     assertEquals('O', jogo.obterPosicao(1, 1));
   }
 
+  @Test(expected = IllegalArgumentException.class)
+  public void testElementoValido (){
+    jogo.preenchePosicao(1, 1, 'f');
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testPosicaoJaPreenchida (){
+    jogo.preenchePosicao(1, 1, 'O');
+    jogo.preenchePosicao(1, 1, 'X');
+  }
+  @Test(expected = IllegalArgumentException.class)
+  public void testPosicaoInvalidaMaiorQueTabuleiro(){
+    jogo.preenchePosicao(4, 3, 'O');
+  }
+  @Test(expected = IllegalArgumentException.class)
+  public void testPosicaoInvalidaMaiorQueTabuleiro2(){
+    jogo.preenchePosicao(3, 3, 'O');
+  }
+  @Test(expected = IllegalArgumentException.class)
+  public void testPosicaoInvalidaNegativa(){
+    jogo.preenchePosicao(-1, 1, 'X');
+  }
+  
+  @Test
+  public void testTabuleiroCheio() {
+    assertFalse(jogo.checaTabuleiroCheio());
+    jogo.preencheTudo('X');
+    assertTrue(jogo.checaTabuleiroCheio());
+  }
 }
